bf7afa2c4023fca6db00b9f76df75a6a4cfaa6fb
neomiost@gmail.com <neomiost1>
2017-06-13 14:00:51 +0300

    gg

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace WeatherStation
     {
         public static void Main(string[] args)
         {
+
+
             var config = new ConfigurationBuilder()
                 .AddCommandLine(args)
                 .AddEnvironmentVariables(prefix: "ASPNETCORE_")
